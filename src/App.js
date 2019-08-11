c15e410745442612605b6ee6e915ca1e8e090b4e
dennisly <dgranadosl@ucenfotec.ac.cr>
2019-08-10 22:41:36 -0600

    Change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dionisio Movie Theatre
+          Dionisio Movie Theatre?
         </p>
         <a
           className="App-link"
