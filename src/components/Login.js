de8e675d9337240990892cf4715aedd33afd8e71
dennisly <dgranadosl@ucenfotec.ac.cr>
2019-08-24 03:15:25 -0600

    Need to add an await in the Axios Get, in order to validate the username and password.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,117 +1,141 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './../App.css';
 import axios from 'axios';
 
-class Login extends Component{
-    constructor(){
-        super();
-        this.state={
-            email:"",
-            password:"",
-        };
-        this.handleInputChange  = this.handleInputChange.bind(this);
-        this.handleLogin        = this.handleLogin.bind(this);
-    }
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
 
-    handleLogin(e) {
-        e.preventDefault();
-        /*this.props.onAddTodo(this.state);*/
-        if(this.state.email !== "" &&
-        this.state.password !== ""){
-          FuncLogin();
-        }else{
-            alert("Olvidaste llenar uno de los campos!!!");
-        }
-        this.setState({
-            email: ''
-        });
+  handleLogin(e) {
+    e.preventDefault();
+    /*this.props.onAddTodo(this.state);*/
+    if (this.state.email !== "" &&
+      this.state.password !== "") {
+      FuncLogin();
+    } else {
+      alert("Olvidaste llenar uno de los campos!!!");
     }
+    this.setState({
+      email: '',
+      password: ''
+    });
+  }
 
-    handleInputChange(e) {
-        const {value, name} = e.target;
-        console.log(value, name);
-        this.setState({
-          [name]: value
-        });
-    }
+  handleInputChange(e) {
+    const { value, name } = e.target;
+    console.log(value, name);
+    this.setState({
+      [name]: value
+    });
+  }
 
-    render(){
-      return (
-        <div className="App">
-          <div className="container" id="login-container">
-            <div className="col align-items-center">
-              <div className="card">
-                <form className="card-body">
-                  <div className="form-group">
-                    <input
-                      id="email"
-                      type="text"
-                      name="email"
-                      className="form-control"
-                      value={this.state.email}
-                      onChange={this.handleInputChange}
-                      placeholder="Email"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <input
-                      id="password"
-                      type="password"
-                      name="password"
-                      className="form-control"
-                      value={this.state.password}
-                      onChange={this.handleInputChange}
-                      placeholder="Password"
-                    />
-                  </div>
-                  <div className="row">
-                    <button
-                      id="buttonLogin"
-                      //type="submit"
-                      className="btn btn-info btn-block"
-                      onClick={this.handleLogin}
-                      >
-                      Login
+  render() {
+    return (
+      <div className="App">
+        <div className="container" id="login-container">
+          <div className="col align-items-center">
+            <div className="card">
+              <form className="card-body">
+                <div className="form-group">
+                  <input
+                    id="email"
+                    type="text"
+                    name="email"
+                    className="form-control"
+                    value={this.state.email}
+                    onChange={this.handleInputChange}
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    id="password"
+                    type="password"
+                    name="password"
+                    className="form-control"
+                    value={this.state.password}
+                    onChange={this.handleInputChange}
+                    placeholder="Password"
+                  />
+                </div>
+                <div className="row">
+                  <button
+                    id="buttonLogin"
+                    //type="submit"
+                    className="btn btn-info btn-block"
+                    onClick={this.handleLogin}
+                  >
+                    Login
                     </button>
-                  </div>
-                </form>
-              </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
-      );
-    }
+      </div>
+    );
+  }
 
 }
 
 
-function FuncLogin(){
+function FuncLogin() {
 
+  var checker = 0;
   let email = document.getElementById("email").value;
   var ROOT = "http://dionisio-env.yenwtnrkxn.us-east-1.elasticbeanstalk.com/";
-  var URL = ROOT + "GetAccount/"+email;
-  console.log(URL);
-  axios.get(URL).then(res => {
-    if(res.status == 200){
-      sessionStorage.setItem("id", res.data.account_ID);
-      sessionStorage.setItem("usuario", res.data.name_ACCOUNT);
-      sessionStorage.setItem("email", res.data.email);
-      sessionStorage.setItem("role", res.data.role_ACCOUNT);
+  var URL = ROOT + "GetAccount/" + email;
+  var usuario;
+  var id;
+  var emailAccount;
+  var role;
 
-      URL = ROOT + "/getEmailByID/"+ email;
-      axios.get().then(res => {
-
-        console.log(res);
-
-      });
+  var temp;
 
 
-    }else{
+   axios.get(URL).then(res => {
+    temp = res;
+
+    if (res.status == 200) {
+      checker = 1;
+      console.log("El valor de " + checker + "jaja");
+      usuario = res.data.name_ACCOUNT;
+      id = res.data.account_ID;
+      emailAccount = res.data.email;
+      role = res.data.role_ACCOUNT;
+      console.log(id + " " + usuario + " " + emailAccount + " " + role + " " + checker);
+
+    } else {
       alert("Correo no registrado.");
     }
-  
+
   });
-  
+
+  console.log("El valor despues del primer GET: " + checker);
+
+  if (checker == 1) {
+    URL = ROOT + "GetEmail/" + email;
+    axios.get(URL).then(res => {
+      if (res.status = 200) {
+        console.log(res);
+        sessionStorage.setItem("id", id);
+        sessionStorage.setItem("usuario", usuario);
+        sessionStorage.setItem("email", emailAccount);
+        sessionStorage.setItem("role", role);
+        alert("Sesion iniciada.");
+      } else {
+        alert("Contraseña incorrecta.")
+      }
+    });
+  }
+
 }
 
 
